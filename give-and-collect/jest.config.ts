bab48ca0a4d945230b1a3a0a14302990eb430253
jtrancart <jtrancart@sqli.com>
2024-06-11 15:48:29 +0200

    test: :test_tube: add failing footer test

---
+++ give-and-collect/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default createJestConfig(config);
