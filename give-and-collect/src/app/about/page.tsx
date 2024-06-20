5206426250bf4bfdb4bd605028cd9cbaa9f0de2b
Marina ASHRAF MORIS <119459259+Marina-ASH@users.noreply.github.com>
2024-06-21 01:44:54 +0200

    fix: :sparkles: page about us

--- give-and-collect/src/app/about/page.tsx
+++ give-and-collect/src/app/about/page.tsx
@@ -10,23 +10,44 @@ import {
     TextField,
     Card,
     CardMedia,
-    CardContent,
-    useTheme,
-    useMediaQuery,
     Paper,
-    Avatar
+    styled
 } from '@mui/material';
 import Carousel from 'react-material-ui-carousel';
 import { Send } from '@mui/icons-material';
 
-const About: React.FC = () => {
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+interface FormValues {
+    name: string;
+    email: string;
+    message: string;
+}
 
-    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+const initValues: FormValues = {
+    name: '',
+    email: '',
+    message: '',
+};
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+// Exemple de style inline avec sx
+const PaperStyled = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(2),
+    '& .MuiTextField-root': {
+        marginBottom: theme.spacing(2),
+    },
+    '& .MuiButton-root': {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export default function Contact() {
+    const [formData, setFormData] = useState<FormValues>(initValues);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
     };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -41,7 +62,7 @@ const About: React.FC = () => {
             });
             if (response.ok) {
                 alert('Message envoyé avec succès !');
-                setFormData({ name: '', email: '', message: '' });
+                setFormData(initValues);
             } else {
                 alert("Erreur lors de l'envoi du message.");
             }
@@ -53,32 +74,37 @@ const About: React.FC = () => {
 
     return (
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            {/* À propos de nous */}
             <Typography variant="h3" align="center" gutterBottom>
-                Qui sommes-nous ?
+                À propos de nous
             </Typography>
             <Grid container spacing={4} justifyContent="center" alignItems="center">
                 <Grid item xs={12} md={6}>
-                    <Typography variant="body1" paragraph>
-                        Nous sommes deux développeurs étudiants de CESI et dans le cadre de notre projet citoyen, nous avons décidé de créer une plateforme permettant de mettre en relation des personnes pour la collecte de vêtements. Notre objectif est de faciliter la communication et la collaboration entre les individus pour une cause commune : aider ceux qui en ont besoin en collectant et redistribuant des vêtements.
-                    </Typography>
+                    <PaperStyled elevation={3}>
+                        <Typography variant="body1">
+                            Nous sommes deux développeurs étudiants de CESI et dans le cadre de notre projet citoyen, nous avons décidé de créer une plateforme permettant de mettre en relation des personnes pour la collecte de vêtements. Notre objectif est de faciliter la communication et la collaboration entre les individus pour une cause commune : aider ceux qui en ont besoin en collectant et redistribuant des vêtements.
+                        </Typography>
+                    </PaperStyled>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <Card>
                         <CardMedia
                             component="img"
-                            image="../../public/assests/images/banner.jpg"
+                            height="auto"
+                            image="/assets/images/image-4.jpg"
                             alt="Nous"
                         />
                     </Card>
                 </Grid>
             </Grid>
 
+            {/* Carrousel d'images */}
             <Typography variant="h4" align="center" gutterBottom sx={{ mt: 4 }}>
                 Notre projet en images
             </Typography>
             <Paper elevation={3} sx={{ p: 2, mb: 4 }}>
                 <Carousel>
-                    {['../../public/assests/images/image-1.jpg', '/images/image-2.jpg', '/images/image-3.jpg', '/images/image-4.jpg'].map((image, index) => (
+                    {['/assets/images/image-1.jpg', '/assets/images/image-2.jpg', '/assets/images/image-3.jpg', '/assets/images/image-4.jpg'].map((image, index) => (
                         <Card key={index}>
                             <CardMedia
                                 component="img"
@@ -91,55 +117,63 @@ const About: React.FC = () => {
                 </Carousel>
             </Paper>
 
-            <Typography variant="h4" align="center" gutterBottom sx={{ mt: 4 }}>
+            {/* Contactez-nous */}
+            <Typography variant="h3" align="center" gutterBottom>
                 Contactez-nous
             </Typography>
-            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 4, mb: 4 }}>
-                <Paper elevation={3} sx={{ p: 3 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Nom"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Email"
-                                name="email"
-                                type="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="Message"
-                                name="message"
-                                multiline
-                                rows={4}
-                                value={formData.message}
-                                onChange={handleChange}
-                                required
-                            />
-                        </Grid>
-                        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-                            <Button variant="contained" color="primary" type="submit" endIcon={<Send />}>
-                                Envoyer
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Box>
+            <Grid container spacing={4} justifyContent="center" alignItems="center">
+                <Grid item xs={12} md={6}>
+                    <PaperStyled elevation={3}>
+                        <form onSubmit={handleSubmit}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Nom"
+                                        name="name"
+                                        value={formData.name}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Email"
+                                        name="email"
+                                        type="email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Message"
+                                        name="message"
+                                        multiline
+                                        rows={4}
+                                        value={formData.message}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        type="submit"
+                                        endIcon={<Send />}
+                                    >
+                                        Envoyer
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </PaperStyled>
+                </Grid>
+            </Grid>
         </Container>
     );
-};
-
-export default About;
+}
--- give-and-collect/src/app/api/contact/route.ts
+++ give-and-collect/src/app/api/contact/route.ts
@@ -1,43 +1,68 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-import {transporter, mailOptions} from '../../../types/nodemailer';
+import { EmailContent, MailOptions, CustomTransporter } from '../../../types/sendMail';
 
-dotenv.config();
+const CONTACT_MESSAGE_FIELDS: { [key: string]: string } = {
+    name: 'Name',
+    email: 'Email',
+    message: 'Message',
+};
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+const transporter: CustomTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+}) as CustomTransporter;
+
+const generateEmailContent = (data: {
+    name: string;
+    email: string;
+    message: string;
+}): EmailContent => {
+    const stringData = Object.entries(data).reduce(
+        (str, [key, val]) =>
+            (str += `${CONTACT_MESSAGE_FIELDS[key]}: \n${val} \n \n`),
+        ''
+    );
+    const htmlData = Object.entries(data).reduce((str, [key, val]) => {
+        return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
+    }, '');
+
+    return {
+        text: stringData,
+        html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+    };
+};
+
+export async function POST(req: NextRequest) {
     try {
-        const { name, email, message } = req.body;
+        const body = await req.json();
+        const { name, email, message } = body;
 
         if (!name || !email || !message) {
-            return res.status(400).json({ error: 'Tous les champs doivent être remplis' });
+            return NextResponse.json({ message: 'Bad request' }, { status: 400 });
         }
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
+        const userEmail = process.env.EMAIL_USER;
+        if (!userEmail) {
+            throw new Error('EMAIL_USER environment variable is not defined');
+        }
 
-        const mailOptions = {
-            from: email,
-            to: process.env.EMAIL_USER,
-            subject: `Message de ${name}`,
-            text: message,
+        const mailOptions: MailOptions & EmailContent = {
+            from: userEmail,
+            to: userEmail,
+            subject: `New message from ${name}`,
+            ...generateEmailContent(body),
         };
 
         await transporter.sendMail(mailOptions);
 
-        return res.status(200).json({ message: 'Email envoyé avec succès' });
+        return NextResponse.json({ success: true }, { status: 200 });
     } catch (error) {
-        console.error('Erreur lors de l\'envoi de l\'email:', error);
-        return res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'email' });
+        console.error('Error sending email:', error);
+        return NextResponse.json({ message: 'Email sending failed' }, { status: 500 });
     }
 }
 
-export function GET(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Méthode ${req.method} non autorisée`);
-}
