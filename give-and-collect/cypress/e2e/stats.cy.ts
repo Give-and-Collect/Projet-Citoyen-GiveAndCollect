80564712a5483161fce096ffb33e2e5305d967c3
Marina ASHRAF MORIS <119459259+Marina-ASH@users.noreply.github.com>
2024-07-29 11:49:39 +0200

    fix: :sparkles: test for stats

---
+++ give-and-collect/cypress/e2e/stats.cy.ts
@@ -0,0 +1,22 @@
+describe('Statistics Page', () => {
+    beforeEach(() => {
+        // Se connecter avant chaque test
+        cy.visit('http://localhost:3000/login');
+        cy.get('input[name="email"]').type(Cypress.env('CYPRESS_EMAIL'), { delay: 100 });
+        cy.get('input[name="password"]').type(Cypress.env('CYPRESS_PASSWORD'), { delay: 100 });
+        cy.get('button').contains('Se connecter').click();
+        cy.url().should('eq', 'http://localhost:3000/');
+    });
+
+    it('Test the consultation of statistics', () => {
+        cy.visit('http://localhost:3000/admin/stats');
+
+        // Vérifier que les cartes statistiques sont présentes et affichent les labels corrects
+        cy.contains('Nombre d\'annonces').should('be.visible');
+        cy.contains('Nombre d\'événements').should('be.visible');
+        cy.contains('Nombre d\'utilisateurs').should('be.visible');
+        cy.contains('Nombre de partages').should('be.visible');
+        cy.contains('Nombre de visites').should('be.visible');
+        cy.contains('Position dans les résultats de recherche').should('be.visible');
+    });
+});
